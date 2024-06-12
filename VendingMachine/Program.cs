9c21b9904b31f54d9a838009f7e973b04b88ee72
Justin Rinehuls <jrinehuls@gmail.com>
2024-06-11 22:24:44 -0400

    move solution out of project directory

--- VendingMachine/Program.cs
+++ VendingMachine/Program.cs
@@ -7,7 +7,7 @@ using VendingMachine.Services.Impl;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddDbContext<DataContext>(options => 
+builder.Services.AddDbContext<DataContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
 
